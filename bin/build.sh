c16bd73a98d472a445ee2a25d1695d6adfcf1e3e
sergiusd <sergiusd@ya.ru>
2023-11-17 13:30:41 +0400

    Improved the build script.

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,16 @@
 set -ex
 
-TGZ_NAME=jitsi-excalidraw-0.0.16.1.tgz
+TGZ_NAME="jitsi-excalidraw-0.0.16.$(date +%s).tgz"
 
 yarn
 cd src/packages/excalidraw
 yarn
 yarn run build:umd
-rm -rf tgz
-mkdir tgz
-cp -r {dist,types,README.md,main.js,package.json} tgz/
-cd tgz
-tar -czf $TGZ_NAME --exclude=$TGZ_NAME ./*
-cd ../../../..
+rm -rf package
+mkdir package
+cp -r {dist,types,README.md,main.js,package.json} package/
+tar -czf $TGZ_NAME --exclude=$TGZ_NAME ./package
+cd ../../..
 rm -rf bin/*.tgz
-mv src/packages/excalidraw/tgz/$TGZ_NAME bin/
+mv src/packages/excalidraw/$TGZ_NAME bin/
 echo "Use file://$(pwd)/bin/$TGZ_NAME"
